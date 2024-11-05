114eb9f5bbca94f98b9a3f02a7b8d57c1990f680
mdarifuzzaman <mdarifuzzamanshakil12@gmail.com>
2024-11-05 23:50:07 +0600

    Express js Backend Blog Project

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+import * as blogController from "../app/controllers/blogController.js";
+
+// create route
+ router.post("/create-blog", blogController.createBlog);
+
+//Read Route
+router.get("/read-blog", blogController.readBlog);
+
+// Update Route
+router.put("/update-blog", blogController.updateBlog);
+
+// Delete Route
+router.delete("/delete-blog", blogController.deleteBlog);
+
+
+
+
+export default router;
